4d937b5feb1c43b01debffa80524213943aeebb4
Courtney Taylor <courtneytaylor@college.harvard.edu>
2023-09-29 19:53:35 -0400

    Updated my name and ideas

---
+++ Gov 50 Final Project.R
@@ -0,0 +1,4 @@
+Title: Gov 50 Final Project
+Author: Courtney Taylor
+
+Project Plan: To analyze the corrleation between the percentage of men who come to women's sporting events and the amount of coverage women's sports get on sports stations.
